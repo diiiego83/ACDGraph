bed7e1786f4b272e7ebec2ed7b1cf82fa9a3c256
Diego Molteni <DMolteni3@slb.com>
2018-08-07 16:18:37 +0100

    added local loading

--- src/app/acdgraph/acdgraph.component.ts
+++ src/app/acdgraph/acdgraph.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 import { ColorPoint, ColorMap, CMapModel } from '../lib/colormap';
 
-import { DataReader, ENDIANITY } from '../lib/datareader';
+import { DataReader } from '../lib/datareader';
 
 @Component({
   selector: 'app-acdgraph',
@@ -16,7 +16,7 @@ export class AcdgraphComponent implements AfterViewInit {
 
   private _cs: HTMLCanvasElement;
   private _cx: CanvasRenderingContext2D;
-  public _cs_autosize = true;
+  public _cs_autosize = false;
   public file: any;
   private _image: ImageBitmap;
   private _zoom = 1.0;
@@ -36,78 +36,29 @@ export class AcdgraphComponent implements AfterViewInit {
       this._cs.width = 0;
       this._cs.height = 0;
     }
-    // this._cs.style.borderColor = 'transparent';
-    // this._cx.clearRect(0, 0, this._cs_w, this._cs_h);
-  }
-
-  private test1() {
-    this.clear();
-
-    const pt1: ColorPoint = {
-      color: { red: 0, blue: 0, green: 0 },
-      pos: 0
-    };
-    const pt2: ColorPoint = {
-      color: { red: 153, blue: 153, green: 153 },
-      pos: 100
-    };
-    // const cmap = ColorMap.custom([pt1, pt2]);
-    const cmap = ColorMap.create(CMapModel.RED);
-
-    const nw = 50;
-    const nh = 400;
-    const imagedata = this._cx.createImageData(nw, nh);
-    const ratio = 256.0 / nh;
-    for (let jj = 0; jj < nh; jj++) {
-      const idref = Math.ceil((jj + 1) * ratio) - 1;
-      for (let ii = 0; ii < nw; ii++) {
-        const idx = jj * (nw * 4) + ii * 4;
-        imagedata.data[idx + 0] = (cmap[idref] >> 0) & 0xff;
-        imagedata.data[idx + 1] = (cmap[idref] >> 8) & 0xff;
-        imagedata.data[idx + 2] = (cmap[idref] >> 16) & 0xff;
-        imagedata.data[idx + 3] = (cmap[idref] >> 24) & 0xff; // alpha always 255 (0xff)
-      }
-    }
-
-    const daddy: HTMLDivElement = this.daddy.nativeElement;
-    if (this._cs_autosize) {
-      this._cs.height = Math.min(nh, daddy.clientHeight - 2);
-      this._cs.width = Math.min(nw, daddy.clientWidth - 2);
-    } else {
-      this._cs.height = daddy.clientHeight - 2;
-      this._cs.width = daddy.clientWidth - 2;
-    }
-    // const nw = 50;
-    // const nh = 600;
-    // const imagedata = this._cx.createImageData(nw, nh);
-    // for (let jj = 0; jj < nw * nh; jj++) {
-    //   imagedata[jj * 4 + 0] = 255;
-    //   imagedata[jj * 4 + 1] = 0;
-    //   imagedata[jj * 4 + 2] = 0;
-    //   imagedata[jj * 4 + 3] = 255;
-    // }
-
-    // this.cx.putImageData(imagedata, 0, 0);
-    createImageBitmap(imagedata, 0, 0, nw, nh).then(image => {
-      this._cx.drawImage(image, 0, 0);
-    });
   }
 
 
   private test() {
-
-    this.testcore();
-
+    const oReq = new XMLHttpRequest();
+    oReq.open('GET', '/vel_563x3937.raw', true);
+    oReq.responseType = 'arraybuffer';
+    oReq.onload = (oEvent) => {
+      const arrayBuffer = oReq.response; // Note: not oReq.responseText
+      if (arrayBuffer) {
+        const data: Float32Array = DataReader.readfloat32(arrayBuffer);
+        this.render(data);
+      }
+    };
+    oReq.send(null);
   }
-
 
   openFile(event) {
     const input = event.target;
     const reader = new FileReader();
     reader.onload = () => {
-      // const data1: Float32Array = DataReader.readfloat32(reader.result, ENDIANITY.BIG_ENDIAN);
-      const data2: Float32Array = DataReader.readfloat32(reader.result);
-      this.render(data2);
+      const data: Float32Array = DataReader.readfloat32(reader.result);
+      this.render(data);
     };
     reader.readAsArrayBuffer(input.files[0]);
   }
@@ -125,11 +76,15 @@ export class AcdgraphComponent implements AfterViewInit {
     };
     let cmap: number[];
     // if (true) {
-      // cmap = ColorMap.custom([pt1, pt2]);
+      const cmap1 = ColorMap.custom([pt1, pt2]);
     // }
     // else {
-      cmap = ColorMap.create(CMapModel.REDGRAY);
+      cmap = ColorMap.create(CMapModel.BLACKWHITE);
     // }
+
+    console.log(cmap1);
+    console.log('xxxxxxxxxxxxxxxxxxxxxxx');
+    console.log(cmap);
     const ncol = 3937;
     const nrow = 563;
     // const nw = 563;
@@ -176,17 +131,6 @@ export class AcdgraphComponent implements AfterViewInit {
     });
   }
 
-//   // Code from https://developer.mozilla.org/En/Using_XMLHttpRequest#Receiving_binary_data
-//  public load_binary_resource(url) {
-//   const req = new XMLHttpRequest();
-//   req.open('GET', url, false);
-//   // The following line says we want to receive data as Binary and not as Unicode
-//   req.overrideMimeType('text/plain; charset=x-user-defined');
-//   req.send(null);
-//    if (req.status !== 200) { return ''; }
-//   return req.responseText;
-// }
-
   private plus() {
     this._zoom += 0.05;
     this._cx.clearRect(0, 0, this._cs.width, this._cs.height);
@@ -205,37 +149,4 @@ export class AcdgraphComponent implements AfterViewInit {
     this._cx.drawImage(this._image, 0, 0, this._cs.width, this._cs.height);
   }
 
-  private testcore() {
-    this.clear();
-
-    const cmap = ColorMap.create(CMapModel.RED);
-
-    const nw = 500;
-    const nh = 400;
-    const imagedata = this._cx.createImageData(nw, nh);
-    const ratio = 256.0 / nh;
-    for (let jj = 0; jj < nh; jj++) {
-      const idref = Math.ceil((jj + 1) * ratio) - 1;
-      for (let ii = 0; ii < nw; ii++) {
-        const idx = jj * (nw * 4) + ii * 4;
-        imagedata.data[idx + 0] = (cmap[idref] >> 0) & 0xff;
-        imagedata.data[idx + 1] = (cmap[idref] >> 8) & 0xff;
-        imagedata.data[idx + 2] = (cmap[idref] >> 16) & 0xff;
-        imagedata.data[idx + 3] = (cmap[idref] >> 24) & 0xff; // alpha always 255 (0xff)
-      }
-    }
-
-    const daddy: HTMLDivElement = this.daddy.nativeElement;
-    if (this._cs_autosize) {
-      this._cs.height = Math.min(nh, daddy.clientHeight - 2);
-      this._cs.width = Math.min(nw, daddy.clientWidth - 2);
-    } else {
-      this._cs.height = daddy.clientHeight - 2;
-      this._cs.width = daddy.clientWidth - 2;
-    }
-
-    createImageBitmap(imagedata, 0, 0, nw, nh).then(image => {
-      this._cx.drawImage(image, 0, 0);
-    });
-  }
 }
